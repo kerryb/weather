1c06031362bd3bbcfb441c5ca83cac9296421387
Kerry Buckley <kerryjbuckley@gmail.com>
2022-03-07 13:09:00 +0000

    Don’t load esbuild when running in target mode

--- weather_ui/mix.exs
+++ weather_ui/mix.exs
@@ -39,7 +39,7 @@ defmodule WeatherUi.MixProject do
       {:phoenix_live_view, "~> 0.17.5"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.6"},
-      {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
+      {:esbuild, "~> 0.3", runtime: Mix.env() == :dev && Mix.target() == :host},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
