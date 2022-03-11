122560949ee52ad3ede87d9445600068a9c54d52
Kerry Buckley <kerryjbuckley@gmail.com>
2022-03-11 16:35:13 +0000

    Sort deps alphabetically

--- weather_firmware/mix.exs
+++ weather_firmware/mix.exs
@@ -31,34 +31,34 @@ defmodule WeatherFirmware.MixProject do
   defp deps do
     [
       # Dependencies for all targets
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:circuits_gpio, "~> 1.0"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:elixir_bme680, "~> 0.2.2"},
       {:nerves, "~> 1.7.15", runtime: false},
-      {:shoehorn, "~> 0.8.0"},
       {:ring_logger, "~> 0.8.3"},
+      {:shoehorn, "~> 0.8.0"},
       {:toolshed, "~> 0.2.13"},
 
       # Poncho dependencies
       {:weather_ui, path: "../weather_ui", targets: @all_targets, env: Mix.env()},
 
       # Dependencies for all targets except :host
-      {:nerves_runtime, "~> 0.11.6", targets: @all_targets},
       {:nerves_pack, "~> 0.6.0", targets: @all_targets},
+      {:nerves_runtime, "~> 0.11.6", targets: @all_targets},
 
       # Dependencies for specific targets
       # NOTE: It's generally low risk and recommended to follow minor version
       # bumps to Nerves systems. Since these include Linux kernel and Erlang
       # version updates, please review their release notes in case
       # changes to your application are needed.
+      {:nerves_system_bbb, "~> 2.13", runtime: false, targets: :bbb},
+      {:nerves_system_osd32mp1, "~> 0.9", runtime: false, targets: :osd32mp1},
       {:nerves_system_rpi, "~> 1.18", runtime: false, targets: :rpi},
       {:nerves_system_rpi0, "~> 1.18", runtime: false, targets: :rpi0},
       {:nerves_system_rpi2, "~> 1.18", runtime: false, targets: :rpi2},
       {:nerves_system_rpi3, "~> 1.18", runtime: false, targets: :rpi3},
       {:nerves_system_rpi3a, "~> 1.18", runtime: false, targets: :rpi3a},
       {:nerves_system_rpi4, "~> 1.18", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.13", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.9", runtime: false, targets: :osd32mp1},
       {:nerves_system_x86_64, "~> 1.18", runtime: false, targets: :x86_64}
     ]
   end
