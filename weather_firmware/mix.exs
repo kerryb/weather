12532179a5d052ebebae7f7c25b33349fe8c5a41
Kerry Buckley <kerryjbuckley@gmail.com>
2022-03-29 10:54:20 +0100

    Remove all nerves systems apart from rpi0 from deps

--- weather_firmware/mix.exs
+++ weather_firmware/mix.exs
@@ -3,7 +3,7 @@ defmodule WeatherFirmware.MixProject do
 
   @app :weather_firmware
   @version "0.1.0"
-  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :osd32mp1, :x86_64]
+  @all_targets [:rpi0]
 
   def project do
     [
@@ -55,19 +55,7 @@ defmodule WeatherFirmware.MixProject do
       {:nerves_runtime, "~> 0.11", targets: @all_targets},
 
       # Dependencies for specific targets
-      # NOTE: It's generally low risk and recommended to follow minor version
-      # bumps to Nerves systems. Since these include Linux kernel and Erlang
-      # version updates, please review their release notes in case
-      # changes to your application are needed.
-      {:nerves_system_bbb, "~> 2.13", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.9", runtime: false, targets: :osd32mp1},
-      {:nerves_system_rpi, "~> 1.18", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.18", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.18", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.18", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.18", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.18", runtime: false, targets: :rpi4},
-      {:nerves_system_x86_64, "~> 1.18", runtime: false, targets: :x86_64}
+      {:nerves_system_rpi0, "~> 1.18", runtime: false, targets: :rpi0}
     ]
   end
 
